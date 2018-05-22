44ca2873963487dc95e1b624fff17dc0ef46811e
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-05-22 16:27:20 +0200

    Enabled start at boot of the fanService class.

--- things/src/main/java/gq/emiliodallatorre/rpi_fan/fanService.java
+++ things/src/main/java/gq/emiliodallatorre/rpi_fan/fanService.java
@@ -5,9 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 
 public class fanService extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent myIntent = new Intent(context, fanController.class);
-        context.startService(myIntent);
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, fanController.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
     }
 }
